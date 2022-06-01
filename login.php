3a60847bb4add978a84488f4b4250e1ced0d2b74
blinea <blineaa@gmail.com>
2022-06-01 13:48:17 -0400

    Diminish tries on failed login

--- login.php
+++ login.php
@@ -8,19 +8,25 @@
         $password = $_GET['password'];
 
         $query = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
+
         $result = $mysql->query($query);
 
         if ($mysql->affected_rows > 0) {
+
             while($row = $result -> fetch_assoc()){
                 $array = $row;
             }
 
             echo json_encode($array);
         }else{
+
+            $query2 = "UPDATE user SET tries = tries - 1 WHERE email = '$email'";
+            $result2 = $mysql -> query($query2);
             echo "Incorrect email or password";
         }
 
         $result->close();
+        $result2->close();
         $mysql->close();
 
     }
