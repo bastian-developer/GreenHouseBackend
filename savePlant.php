53715e7897f2ff98a91ede297972f24e5b9029f2
blinea <blineaa@gmail.com>
2022-06-01 22:06:26 -0400

    Hot Fix

--- savePlant.php
+++ savePlant.php
@@ -4,12 +4,13 @@
         
         require_once("db.php");
 
+        $userId = $_POST['userId'];
         $name = $_POST['name'];
         $type = $_POST['type'];
         $origin = $_POST['origin'];
         $photos = $_POST['photos'];
 
-        $query = "INSERT INTO plants (name, type, origin, photos) VALUES ('$name', '$type', '$origin', '$photos')";
+        $query = "INSERT INTO plant (userId, name, type, origin, photos) VALUES ('$userId','$name', '$type', '$origin', '$photos')";
         $result = $mysql -> query($query);
 
         if($result === TRUE) {
