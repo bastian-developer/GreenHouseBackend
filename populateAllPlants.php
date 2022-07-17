7a9c4dac5fb8feb0c4cb0dfa5664fb5ad4359e86
blinea <blineaa@gmail.com>
2022-07-16 23:51:45 -0400

    Fill Global Plant List

---
+++ populateAllPlants.php
@@ -0,0 +1,42 @@
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        
+    require_once("db.php");
+
+
+    $query = "SELECT * FROM plant";
+
+    $result = $mysql->query($query);
+
+    if ($mysql->affected_rows > 0) {
+
+        $return_arr['plants'] = array();
+
+        while($row = $result -> fetch_array()){
+
+            array_push($return_arr['plants'], array(
+                'id'=>$row['id'],
+                'userId'=>$row['userId'],
+                'name'=>$row['name'],
+                'type'=>$row['type'],
+                'origin'=>$row['origin'],
+                'photos'=>$row['photos'],
+            ));
+        }
+
+        echo json_encode($return_arr);
+
+    }else{
+
+        echo "Found Nothing";
+
+    }
+
+    $result->close();
+    
+    $mysql->close();
+
+}
+
+?>
